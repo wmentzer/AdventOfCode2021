5446b80241d0b76ed240958f2bcc0715e9442ce0
Will Mentzer <32285343+wmentzer@users.noreply.github.com>
2021-12-07 21:31:28 +0000

    Progress on Part 2

--- Day3/Part2/main.py
+++ Day3/Part2/main.py
@@ -1,13 +1,51 @@
 #!/usr/bin/env python3
 
 def main():
-  inputList = []
-  co2 = []
   o2 = []
-  col = 0
+  co2 = o2
+  col, one, zero = 0, 0, 0
 
   with open("./Day3/Part2/input.txt", 'r') as f:
-    inputList = f.read().split()
+    o2 = f.read().split()
+
+  while len(o2) > 1:
+    print(col, o2)
+    for x in o2:
+      if(o2[col] == '1'):
+        one += 1
+      else:
+        zero += 1
+    
+    if(one > zero):
+      for x in o2:
+        if x[col] == '0':
+          o2.remove(x)
+    else:
+      for x in o2:
+        if x[col] == '1':
+          o2.remove(x)
+    col += 1
+    one, zero = 0, 0
+
+  while len(co2) > 1:
+    for x in co2:
+      if(co2[col] == '1'):
+        one += 1
+      else:
+        zero += 1
+    
+    if(one > zero):
+      for x in co2:
+        if x[col] == '0':
+          co2.remove(x)
+    else:
+      for x in o2:
+        if x[col] == '1':
+          co2.remove(x)
+    col += 1
+    one, zero = 0, 0
+
+  print(co2, o2)
 
 if __name__ == "__main__":
   main()
