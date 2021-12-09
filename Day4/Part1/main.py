e859121958be09c64298ed4781aeb7b1fe6acdd0
Will Mentzer <32285343+wmentzer@users.noreply.github.com>
2021-12-09 17:06:06 +0000

    Added Day 4

---
+++ Day4/Part1/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def main():
+  numList = []
+  boards = []
+  current_board = []
+
+  with open("./Day4/Part1/input.txt", 'r') as f:
+    numList = f.readline()
+    for line in f[2:]:
+      if line != '\n':
+        None
+
+if __name__ == "__main__":
+  main()
