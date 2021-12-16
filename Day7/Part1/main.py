a350449483b7e3f21b25d78d76e23fcb27ca255b
Will Mentzer <32285343+wmentzer@users.noreply.github.com>
2021-12-16 19:31:31 +0000

    Created Day 7

---
+++ Day7/Part1/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+  inputList = []
+
+  with open("./Day7/Part1/input.txt") as f:
+    inputList = f.read().splitlines()
+
+if __name__ == '__main__':
+  main()
